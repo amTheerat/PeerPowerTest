feaa2feae673f9e01c7575256efec6cc91b7461e
Theerat Siripongjinda <theerat.sir@ascendcorp.com>
2020-12-31 15:49:30 +0700

    - config loan module

---
+++ loan/src/main/kotlin/th/test/loan/presentation/calculator/LoanCalculatorFragment.kt
@@ -0,0 +1,18 @@
+package th.test.loan.presentation.calculator
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import th.test.loan.R
+
+class LoanCalculatorFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_loan_calculator, container, false)
+    }
+}
