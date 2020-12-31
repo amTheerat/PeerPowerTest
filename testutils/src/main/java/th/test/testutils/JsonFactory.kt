1d5a2474071fd64a4a55ff4d84e07c48efe1f284
Theerat Siripongjinda <theerat.sir@ascendcorp.com>
2020-12-31 13:44:52 +0700

    - create core module - create base_module_config.gradle, dependencies.gradle - create settings.gradle for module setting - create testutils module

---
+++ testutils/src/main/java/th/test/testutils/JsonFactory.kt
@@ -0,0 +1,19 @@
+package th.test.testutils
+
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
+
+object JsonFactory {
+    fun getStringFromJsonTestResource(path: String): String {
+        val source = javaClass.getResourceAsStream("/$path")
+        val bis = BufferedInputStream(source)
+        val buf = ByteArrayOutputStream()
+        var result = bis.read()
+        while (result != -1) {
+            buf.write(result.toByte().toInt())
+            result = bis.read()
+        }
+        val jsonText = buf.toString("UTF-8")
+        return jsonText
+    }
+}
